8fce24c7197ffe97488959174188f8f8cd40b183
MariaJoseParedes <mjparedes.oltra@gmail.com>
2026-03-30 18:39:53 -0300

    Implementacion de arquitectura del tablero, layout principal y metodo gameBoard con gridView.

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,75 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Buscaminas',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Text('Hello, World!'), // Apuntamos a nuestra nueva pantalla
+      home: const MinesweeperScreen(), // Apuntamos a nuestra nueva pantalla
+    );
+  }
+}
+
+class MinesweeperScreen extends StatelessWidget {
+  const MinesweeperScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buscaminas')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Área de Status
+            Container(
+              height: 60,
+              color: const Color.fromARGB(176, 224, 224, 224),
+              child: const Center(
+                child: Text(
+                  'STATUS: 349 segundos | Minas: 10 | Cuadros: 56',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+            ),
+            const Divider(height: 1),
+            // Área de Juego
+            Expanded(
+              //Expande el tablero para llenar la pantalla
+              child: _gameBoard(),
+            ), // Aquí llamamos al tablero
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _gameBoard() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AspectRatio(
+          aspectRatio: 1.0, // Cuadrado perfecto
+          child: GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8, // 8 columnas
+              crossAxisSpacing: 2.0,
+              mainAxisSpacing: 2.0,
+            ),
+            itemCount: 64, // 8x8 = 64 celdas
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey[400],
+                  border: Border.all(color: Colors.blueGrey[600]!, width: 1.5),
+                ),
+              ); // Cada celda es un widget
+            },
+          ),
+        ),
+      ),
     );
   }
 }
