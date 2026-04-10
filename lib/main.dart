e857023647421aede70785aab100df74c7745ae4
MariaJoseParedes <mjparedes.oltra@gmail.com>
2026-04-10 12:03:12 -0400

    creación de nueva rama structure and assets, instalacion de flutter y uso de logger en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'; //para los iconos jje
 import 'package:google_fonts/google_fonts.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 void main() {
+  logger.d('Iniciando la aplicación de Buscaminas'); // Debug
+  logger.i('Iniciando la aplicación de Buscaminas'); // Info
+  logger.w('Iniciando la aplicación de Buscaminas'); // Warning
+  logger.e('Iniciando la aplicación de Buscaminas'); // Error
   runApp(const MyApp());
 }
 
